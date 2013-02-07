b3bb2444ad4ced43ec4a5a9644ba4a5fe6324a90
abhiramj <abhiramj@gmail.com>
2013-02-07 18:58:36 -0500

    Update src/lucenesearch/lucene_search.py
    Added comments to Lucene search

--- src/lucenesearch/lucene_search.py
+++ src/lucenesearch/lucene_search.py
@@ -1,23 +1,36 @@
-import logging
+#!/usr/bin/env python
+'''
 
+This script searches over the lucene index for a search term  
+
+Created On: Jan 28, 2013 
+Created By: Abhiram J 
+  
+'''
+import logging
 import lucene
 import os
 from lucene import \
     SimpleFSDirectory,File, \
     StandardAnalyzer, IndexSearcher, Version, QueryParser
 
+# Setting up logger
 logger = logging.getLogger('lucene_search')
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',level=logging.INFO,
                     delay=True)
 def lucene_search(index_dir, limit, query_text):
-    
+    '''
+    lucene_search: Search a built index and return upto limit number of responses 
+    Arguments: Input index folder, limit value of results returned, query(as string)
+    Returns: paths of responsive files as list
+    '''
     
     logging.basicConfig(file=os.path.join(index_dir,"lucene_search.log"))
     logger.info("Initializing search....")
     lucene.initVM()
     logger.info("Reading index from "+index_dir)
     index = SimpleFSDirectory(File(index_dir))
-    analyzer = StandardAnalyzer(Version.LUCENE_30)
+    analyzer = StandardAnalyzer(Version.LUCENE_30) #Lucene version used to generate index
     searcher = IndexSearcher(index)
     
     logger.info("Parsing query :"+ query_text)
@@ -28,7 +41,9 @@ def lucene_search(index_dir, limit, query_text):
     hit_paths = []
 
     for hit in hits.scoreDocs:
-        #print hit.score, hit.doc, hit.toString()
+        # The following code also generates score for responsive/found documents and the 
+        # content index which matched
+        # print hit.score, hit.doc, hit.toString()
         doc = searcher.doc(hit.doc)
         hit_paths.append(doc.get("path"))
     
