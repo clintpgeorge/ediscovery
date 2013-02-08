770715c310e6460e6ebfd78ab2689e332cea8c90
abhiramj <abhiramj@gmail.com>
2013-02-08 14:49:06 -0500

    testing ed.py to use lucene on neo

--- src/ed.py
+++ src/ed.py
@@ -12,9 +12,9 @@ import os
 import datetime
 import numpy as np 
 
-#from lucenesearch.lucene_index import lucene_index
-#from lucenesearch.lucene_search import lucene_search  
-from tm.lda_process_query import process_query, load_docs_info, load_lda_variables
+from lucenesearch.lucene_index import lucene_index
+from lucenesearch.lucene_search import lucene_search  
+#from tm.lda_process_query import process_query, load_docs_info, load_lda_variables
 from sampler.random_sampler import random_sampler
 from file_utils import copy_random_files, find_files_in_folder, copy_files_with_dir_tree
 
@@ -26,7 +26,7 @@ Global variables
 TODO: The hard coded values should be removed 
 '''
 
-DATA_PATH = '/home/cgeorge/ediscovey/enron_mail_20110402'# '/data/ediscovery/enron' # raw_input('Data path: ')
+DATA_PATH = '/media/Non OS/EDiscovery/enron_mail_20110402'# '/data/ediscovery/enron' # raw_input('Data path: ')
 dictionary_file = os.path.join(DATA_PATH, 'fs_enron.dict')
 doc_paths_file = os.path.join(DATA_PATH, 'fs_enron.email_paths')
 lda_mdl_file = os.path.join(DATA_PATH, 'fs_enron.lda_mdl')
@@ -35,14 +35,15 @@ SEEDCONSTANT = 2013
 
 # Loads the LDA model and file details 
 
-doc_paths = load_docs_info(doc_paths_file)
-dictionary, lda, index = load_lda_variables(dictionary_file, lda_mdl_file, lda_index_file)
+#doc_paths = load_docs_info(doc_paths_file)
+#dictionary, lda, index = load_lda_variables(dictionary_file, lda_mdl_file, lda_index_file)
 
 #Lucene specific settings and indexing 
 output_folder = raw_input('Enter output folder: ')
 choice = 'n'
-#lucene_index_file = os.path.join(output_folder, 'lucene.index')
-#lucene_index(DATA_PATH, lucene_index_file)
+lucene_index_file = os.path.join(output_folder, 'lucene.index')
+if not os.path.exists(lucene_index_file):
+    lucene_index(DATA_PATH, lucene_index_file)
 while choice <> 'y':  
 
     ## Enter query 
@@ -57,19 +58,19 @@ while choice <> 'y':
         None
         # Process the query 
         
-        responsive_docs, non_responsive_docs = process_query(query, dictionary, lda, index, doc_paths, limit)
-        nrd = np.array(non_responsive_docs)
-        nrd_paths = [os.path.join(dir_path, nrd[idx,2]) for idx, dir_path in enumerate(nrd[:,1])] # looks like i'm not getting full file paths
+#        responsive_docs, non_responsive_docs = process_query(query, dictionary, lda, index, doc_paths, limit)
+#        nrd = np.array(non_responsive_docs)
+#        nrd_paths = [os.path.join(dir_path, nrd[idx,2]) for idx, dir_path in enumerate(nrd[:,1])] # looks like i'm not getting full file paths
         
     elif search_algorithm == 'Lucene':
-        None 
-#        responsive_docs = lucene_search(lucene_index_file, limit, query)
-#        non_responsive_docs = []
-#        for file_name in find_files_in_folder(DATA_PATH):
-#            if os.path.dirname(file_name) is not lucene_index_file:       # skipping index directory
-#                if file_name not in responsive_docs:
-#                    non_responsive_docs.append(file_name)
-#        nrd_paths=non_responsive_docs 
+#        None 
+        responsive_docs = lucene_search(lucene_index_file, limit, query)
+        non_responsive_docs = []
+        for file_name in find_files_in_folder(DATA_PATH):
+            if os.path.dirname(file_name) is not lucene_index_file:       # skipping index directory
+                if file_name not in responsive_docs:
+                    non_responsive_docs.append(file_name)
+        nrd_paths=non_responsive_docs 
     
     print 'Number of responsive documents:', len(responsive_docs)
     print 'Number of non responsive documents:', len(non_responsive_docs) 
