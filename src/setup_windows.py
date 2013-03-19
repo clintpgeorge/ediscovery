4a4344c8a68261ab7712a5d4d3e9b8b5bca42bc8
Abhiram Jagarlapudi <abhiramj@gmail.com>
2013-03-19 19:30:51 -0400

    changed windows script

--- src/setup_windows.py
+++ src/setup_windows.py
@@ -1,12 +1,4 @@
-from cx_Freeze import setup, Executable
-# Dependencies are automatically detected, but it might need fine tuning.
-build_exe_options = {"packages": ["os","wx","sys"]}
+from distutils.core import setup
+import py2exe
 
-# GUI applications require a different base on Windows (the default is for a
-# console application).
-
-setup(  name = "Random Sampler",
-        version = "0.1",
-        description = "Randomly samples files according to a confidence measure",
-        options = {"build_exe": build_exe_options},
-        executables = [Executable("random_sampler_gui.py")])
+setup(windows=['gui/RandomSampler.py'])
