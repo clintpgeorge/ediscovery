575405b918be4409f3b83a7d23d619728decc280
Abhiram Jagarlapudi <abhiramj@gmail.com>
2013-03-27 13:29:40 -0400

    Changed windows_setup script

--- src/setup_windows.py
+++ src/setup_windows.py
@@ -1,4 +1,10 @@
 from distutils.core import setup
 import py2exe
-
-setup(windows=['gui/RandomSampler.py'])
+setup(
+    windows = [
+        {
+            "script": 'gui/RandomSampler.py',
+            "icon_resources": [(1, 'gui/res/uflaw-edisc1-icon.ico')]
+        }
+    ],
+)
